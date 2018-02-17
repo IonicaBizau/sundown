198b01f86afffae777458ca5a5e9177061cd0913
Ionică Bizău <bizauionica@gmail.com>
2018-02-17 20:15:32 +0200

    Fix formatting problems

--- example/index.js
+++ example/index.js
@@ -7,14 +7,14 @@ const sundownInCluj = sundown(new Date(), 46.7712, 23.6236)
 
 console.log(sundownInCluj)
 // { sunrise:
-//    { raw_time: [ 7, 28 ],
-//      angle: 107.2538519580407,
-//      time: '07:28',
-//      formatted: '7,28, az = 107.2538519580407°' },
+//    { raw_time: [ 7, 27 ],
+//      angle: 106.73357573114593,
+//      time: '07:27',
+//      formatted: '7:27, az = 106.73357573114593°' },
 //   sunset:
-//    { raw_time: [ 17, 52 ],
-//      angle: 252.97122667715888,
-//      time: '07:28',
-//      formatted: '17,52, az = 252.97122667715888°' },
-//   date: 2018-02-16T10:13:25.415Z,
+//    { raw_time: [ 17, 53 ],
+//      angle: 253.492509740339,
+//      time: '17:53',
+//      formatted: '17:53, az = 253.492509740339°' },
+//   date: 2018-02-17T18:15:08.339Z,
 //   coordinates: [ 46.7712, 0.06562111111111112 ] }
